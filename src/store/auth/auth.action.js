5282f03fe62c5ecab0e77ce708334435b00b7f2c
kapkozheckshenov <deadpollkgz@gmail.com>
2020-05-14 21:39:07 +0600

    first fix

--- src/store/auth/auth.action.js
+++ src/store/auth/auth.action.js
@@ -2,7 +2,7 @@ import axiosInstance from "../../api";
 
 export class AuthAction {
   getTestData() {
-    axiosInstance.get(`tasks/`)
-      .then(({data}) => this.tasks = data);
+    axiosInstance.get(`people/`)
+      .then(({data}) => this.tasks = data.results);
   }
 }
