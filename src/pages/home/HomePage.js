1aa2bef290777e2e307c3f2aefe20abf87213a34
Eko <eko13.97@gmail.com>
2020-05-17 21:29:56 +0600

    add modals

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { inject, observer } from "mobx-react";
 // local files
+import brandLogo from '../../assets/images/HandyDay-logo.svg';
+import flagImage from '../../assets/images/Flags.svg';
 import logo from '../../logo.svg';
+import './modal.scss';
 import './style.scss';
 
 export const HomePage = inject("store")(observer(({ store }) => {
@@ -33,6 +36,74 @@ export const HomePage = inject("store")(observer(({ store }) => {
           Learn React
         </a>
       </header>
+
+      <div className="modal __wrapper-block">
+
+        <div className="modal __inner-block">
+          <div className="modal __head">
+            <a className="brand-link" href="#">
+              <img src={brandLogo} alt=""/>
+            </a>
+            <a className="close-btn" href="#">x</a>
+          </div>
+          <div className="modal __body">
+            <h3>Let’s get you signed in</h3>
+            <h6>Type your mobile number. <br/>
+              We’ll text you a code.</h6>
+            <form action="">
+              <div className="field-wrapper">
+                <button className="btn-country"><span><img src={flagImage} alt=""/></span>+996</button>
+                <ul className="list-country">
+
+                  <li className="item-country">
+                    <button>
+                      <span><img src={flagImage} alt=""/></span>+923123123123123123123
+                    </button>
+                  </li>
+                  <li className="item-country">
+                    <button>
+                      <span><img src={flagImage} alt=""/></span>+9
+                    </button>
+                  </li>
+                  <li className="item-country">
+                    <button>
+                      <span><img src={flagImage} alt=""/></span>+123123123
+                    </button>
+                  </li>
+
+                </ul>
+                <input className="field-form __phone" type="number" placeholder="Your number"/>
+              </div>
+              <div className="btn-wrapper">
+                <button className="btn _custom">Text me my code</button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/*<div className="modal __inner-block">*/}
+        {/*  <div className="modal __head">*/}
+        {/*    <a className="brand-link" href="#">*/}
+        {/*      <img src={brandLogo} alt=""/>*/}
+        {/*    </a>*/}
+        {/*    <a className="close-btn" href="#">x</a>*/}
+        {/*  </div>*/}
+        {/*  <div className="modal __body">*/}
+        {/*    <h3>Type the code we sent</h3>*/}
+        {/*    <h6>We just texted you a code. Type in that code here.</h6>*/}
+        {/*    <form action="">*/}
+        {/*      <div className="field-wrapper">*/}
+        {/*        <input className="field-form" type="number" placeholder="*"/>*/}
+        {/*      </div>*/}
+        {/*      <div className="btn-wrapper">*/}
+        {/*        <button className="btn _custom">Text me my code</button>*/}
+        {/*      </div>*/}
+        {/*    </form>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+
+      </div>
+
     </div>
   );
 }));
